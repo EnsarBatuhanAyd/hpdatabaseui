1ba891cee7975c07f41022e620eb545c2db87d00
EnsarBatuhanAyd <ensarbatuhanayd@gmail.com>
2022-09-28 17:35:39 +0300

    content structure and css addition

--- src/Pages/Interface.jsx
+++ src/Pages/Interface.jsx
@@ -1,10 +1,139 @@
 import "./Interface.css";
+import logo from "./../img/logo.png";
 function RealtimePage() {
   return (
     <div className="App">
       <div className="App-Content">
-        <div className="App-Content-Top"><div className="top-bar">TOP</div></div>
-        <div className="App-Content-Bottom"><div className="data-area">DATA</div></div>
+        <div className="App-Content-Top">
+          <div className="top-bar">
+            <div className="logo-area">
+              <img src={logo}></img>
+            </div>
+            <div className="search-area">
+              <input className="search-box"></input>
+            </div>
+            <div className="profile-area">
+              {" "}
+              <div className="profile-box">Welcome User</div>
+              <div className="pimg-box">P</div>
+            </div>
+          </div>
+        </div>
+        <div className="App-Content-Bottom">
+          <div className="data-area">
+            <div className="headers-area">
+              <ul className="table-titles">
+                <li>ID</li>
+                <li>E-Mail</li>
+                <li>Number</li>
+                <li>Name</li>
+                <li>Time</li>
+                <li>Edit/Delete</li>
+              </ul>
+            </div>
+            <hr className="line"></hr>
+            <div className="datas-area">
+              <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+              <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+                 <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+              <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+              <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+
+                <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+                <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+              <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+                <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+                <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+                <ul className="table-datas">
+                <li>63245f3e0a35a8ea987f3e73</li>
+                <li>deneme@gmail.com</li>
+                <li>5354567890</li>
+                <li>isim ve soyismi</li>
+                <li>19 Sep 2022 13.28.07:55</li>
+                <li>Edit/Delete</li>
+              </ul>
+            
+
+            
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
